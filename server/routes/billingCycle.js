b0427b13fa2c6ee6c4b5f3b2a49da178b84b2dd6
Herisson Silva <herissonsilvahs@gmail.com>
2018-09-20 16:54:43 -0300

    billing cycles routes working

--- server/routes/billingCycle.js
+++ server/routes/billingCycle.js
@@ -2,12 +2,11 @@ const BillingCycle = require("../models/BillingCycle");
 const express = require('express')
 const router = express.Router()
 
-BillingCycle.methods(["get", "post", "put", "delete", "patch"]);
+BillingCycle.methods(["get", "post", "put", "delete", "patch"])
 
 module.exports = function (server) {
   server.use('/api', router)
 
-  router.route('/teste').get((request, response, next) => {
-    response.send('teste')
-  })
+  BillingCycle.register(router, '/billingCycles')
+
 }
